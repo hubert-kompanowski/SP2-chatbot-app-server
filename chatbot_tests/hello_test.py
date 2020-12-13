e4a1436f06bfaa794d7cf98e126a5fcd6b8b873e
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2020-12-13 20:06:20 +0100

    Deploy app on heroku

---
+++ chatbot_tests/hello_test.py
@@ -0,0 +1,29 @@
+import json
+
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import AssistantV2
+
+
+def test_response():
+    test_data = ("13.3", "hello")
+
+    config = json.load(open('chatbot_tests/wa_config.json'))
+
+    authenticator = IAMAuthenticator(config['API_key'])
+
+    assistant = AssistantV2(
+        version='2020-04-01',
+        authenticator=authenticator
+    )
+    assistant.set_service_url(config['URL'])
+    response = assistant.message_stateless(
+        assistant_id=config['assistant_id'],
+        input={
+            'message_type': 'text',
+            'text': test_data[1]
+        }
+    ).get_result()
+
+    assert (
+        response['output']['intents'][0]['intent'].startswith(test_data[0])
+    )
